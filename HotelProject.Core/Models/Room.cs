588b1cc3c71455e8ab8f377c18eee79d9da627a9
syazicioglu <suleymanyazicioglu7@gmail.com>
2024-01-27 22:12:16 +0300

    room entity

---
+++ HotelProject.Core/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Core.Models
+{
+    public class Room:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public int BedCount { get; set; }
+        public int BathCount { get; set; }
+        public bool Wifi { get; set; }
+        public string Description { get; set; }
+    }
+}
