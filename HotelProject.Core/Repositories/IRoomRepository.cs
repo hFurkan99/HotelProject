41f3794f87c290c3f09295ff04b8ac124846ab7e
hFurkan <hasanfurkanbaltaci2@gmail.com>
2024-01-27 23:27:58 +0300

    Core Layer

---
+++ HotelProject.Core/Repositories/IRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.Core.Models;
+
+namespace HotelProject.Core.Repositories
+{
+    public interface IRoomRepository : IGenericRepository<Room>
+    {
+        int RoomCount();
+    }
+}
---
+++ HotelProject.Core/Repositories/ISubscribeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.Core.Models;
+
+namespace HotelProject.Core.Repositories
+{
+    public interface ISubscribeRepository : IGenericRepository<Subscribe> { }
+}
